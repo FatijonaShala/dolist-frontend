f4b2a13f1ee94ad7886f18071f7bb99a8d6ec24e
Fatjona <fatjonaash1@gmail.com>
2022-04-27 14:36:09 +0200

    update header and footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import Footerstyle from './Footer.module.scss';
+import style from './Footer.module.scss';
+import { Container, Row, } from 'react-bootstrap';
 
 function Footer() {
-  return (
-    <div className={Footerstyle.footer}>Footer</div>
-  )
+    return ( 
+
+      
+      <footer class="footer py-5">
+        <Container>
+          <Row>
+
+            <div class="col-lg-12 col-12">
+              <p class= {` ${style.copyright_text} text-center`}>Copyright &copy; Dolist . All rights reserved</p>
+              <p class={` ${style.copyright_text} text-center`}>Designed by <a rel="nofollow" href="https://fatijonashala.wordpress.com">Fatijona Shala</a></p>
+            </div>
+          </Row>
+        </Container>
+      </footer>
+      
+           
+
+    
+    )
 }
 
 export default Footer
--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import Headerstyle from './Header.module.scss';
+import { Section, Button, Container, Row, } from 'react-bootstrap';
+import style from './Header.module.scss';
 
 function Header() {
     return (
 
 
-        <div className = { Headerstyle.header }> Header
-        </div>
+        <section className="about full-screen d-lg-flex justify-content-center align-items-center" id="about" >
+            <Container>
+                <Row>
+                    <div className="col-lg-7 col-md-12 col-12 d-flex align-items-center"> 
+                        <div className={style.about_text}>
+                            <small className={`${style.small} ${style.small_text}`}> To do lists
+                                <span class={style.mobile_block}> for busy people! </span>
+                            </small >
+                            <h1 className={`${style.animated} ${style.animated_text}`}>
+                                <span class="mr-2" > we help you to</span>
+                                <div className={style.animated_info}>
+                                    <span className={style.animated_item}>organize </span>
+                                    <span className={style.animated_item}>progress </span>
+                                    <span className={style.animated_item}>focus </span>
+                                </div>
+                            </h1>
+                            <div className={`mt-4 ${style.custom_btn_group}`}>
+                                <a href="login" className={`btn ${style.costum_btn} ${style.costum_btn_bg} ${style.costum_btn_link}`} >
+                                    Create your to do list </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-12 col-12" >
+                        <div className={`${style.about_image} svg`}>
+                            <img src="../img/header.svg" className="img-fluid" alt="svg image" />
+                        </div>
+                    </div>
+
+                </Row>
+            </Container>
+        </section>
+
 
 
 
