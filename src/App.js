dd243871abfc3fa80e50898c87970f7a64348bf0
Fatjona <fatjonaash1@gmail.com>
2022-05-03 17:11:14 +0200

    updating App.js file

--- src/App.js
+++ src/App.js
@@ -7,52 +7,51 @@ import Header from './Pages/Header';
 import Footer from './Pages/Footer';
 import Home from './Pages/Home/';
 import CreateList from './Pages/CreateList';
-import MyCollections from './Pages/My Collections';
+import MyCollections from './Pages/MyCollections';
 import Analytics from './Pages/Analytics';
 import Profile from './Pages/Profile/';
 import About from './Pages/About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-
-
 function App() {
-          
 
-                  
-    const routes = [
-               { path: "/", Component: <Home />, private: false},
-               { path: "/login", Component: <Login />, private: false},
-               { path: "/newList", Component: <CreateList />, private: false},
-               { path: "/myCollections", Component: <MyCollections />, private: false},
-               { path: "/analytics", Component: <Analytics />, private: false},
-               { path: "/profile", Component: <Profile />, private: false},
-               { path: "/about", Component: <About />, private: false}
-                  ];
-      
-                  const listRoutes = routes.map((route, index)=> <Route key={index} path={route.path} element={route.Component} private={route.private} /> )              
+  const routes = [
+    { path: "/", Component: < Home />, private: false },
+    { path: "/login", Component: < Login />, private: false },
+    { path: "/newList", Component: < CreateList />, private: false },
+    { path: "/myCollections", Component: < MyCollections />, private: false },
+    { path: "/analytics", Component: < Analytics />, private: false },
+    { path: "/profile", Component: < Profile />, private: false },
+    { path: "/about", Component: < About />, private: false }
+  ];
 
-  return ( 
-    <div className = "App" >  
-    <Router>
-      <Navbar/>
-      <Header/>
-      <Routes>
-      {listRoutes}
-        {/* <Route path="/login" element={<Login/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/newList" element={<CreateList/>}/>
-        <Route path="/myLists" element={<MyCollections/>}/>
-        <Route path="/analytics" element={<Analytics/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/about" element={<About/>}/> */}
-      </Routes>
-      <Footer/>
-    </Router>
-      </div>
-    
+  const listRoutes = routes.map((route, index) => < Route key={index}
+    path={route.path}
+    element={route.Component}
+    private={route.private}
+  />)
+
+  return (
+    <div className="App" >
+      <Router>
+        <Navbar />
+        <Header />
+        <Routes > {listRoutes} {
+          /* <Route path="/login" element={<Login/>}/>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/newList" element={<CreateList/>}/>
+                  <Route path="/myLists" element={<MyCollections/>}/>
+                  <Route path="/analytics" element={<Analytics/>}/>
+                  <Route path="/profile" element={<Profile/>}/>
+                  <Route path="/about" element={<About/>}/> */
+        }
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+
   );
 }
 
-export default App;
-
+    export default App;
