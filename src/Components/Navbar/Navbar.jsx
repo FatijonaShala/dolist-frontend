b5b4d4ce0e4d3b48aca2fff73ccd3b6cdbdf32a2
Fatjona <fatjonaash1@gmail.com>
2022-05-12 21:19:17 +0200

    updating files

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -32,51 +32,6 @@ const Menu = () => {
       </Navbar>
 
 
-/* <Nav className="navbar navbar-expand-sm navbar-light">
-    <Container>
-        <a className={}"navbar-brand" href="/home"><i className='uil uil-user'></i> DoList</a>
-
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            <span className="navbar-toggler-icon"></span>
-            <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mx-auto">
-                <li className="nav-item">
-                    <a href="/newList" className="nav-link"><span data-hover="CreateList">New List</span></a>
-                </li>
-                <li className="nav-item">
-                    <a href="/myLists" className="nav-link"><span data-hover="myLists">My Lists</span></a>
-                </li>
-                <li className="nav-item">
-                    <a href="/Analytics" className="nav-link"><span data-hover="Analytics">Analytics</span></a>
-                </li>
-                <li className="nav-item">
-                    <a href="/profile" className="nav-link"><span data-hover="Profile">Contact</span></a>
-                </li>
-                <li className="nav-item">
-                    <a href="/about" className="nav-link"><span data-hover="About">About</span></a>
-                </li>
-            </ul>
-
-            <ul className="navbar-nav ml-lg-auto">
-                <div className="ml-lg-4">
-                  <div className="color-mode d-lg-flex justify-content-center align-items-center">
-                    <i className="color-mode-icon"></i>
-                    
-                  </div>
-                </div>
-            </ul>
-        </div>
-    </Container>
-</Nav> */
-
-
-
-
 
 
   
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,68 +1,14 @@
 import React from 'react';
 import styles from './about.module.scss';
-import { Section, Button, Container, Row, } from 'react-bootstrap';
+import {  Button, Container, Row, } from 'react-bootstrap';
 
 function About() {
     return ( 
 
-      <div className={styles.content}>
-        <Container>
-          <Row className="justify-content-center">
-            <div className="col-md-10">
-              <Row className="justify-content-center">
-                <div className="col-md-6">
-
-                  <h3 className={`${styles.heading} mb-4 `}>Let's talk about everything!</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas debitis, fugit natus?</p>
-
-                  <p><img src="images/undraw-contact.svg" alt="Image" className="img-fluid" /></p>
-
-
-                </div>
-                <div className="col-md-6">
-
-                  <form className="mb-5" method="post" id="contactForm" name="contactForm">
-                    <Row>
-                      <div class={`col-md-12 ${styles.form_group}`}>
-                        <input type="text" className={styles.form_control} name="name" id="name" placeholder="Your name" />
-                      </div>
-                    </Row>
-                    <Row>
-                      <div className={`col-md-12 ${styles.form_group}`}>
-                        <input type="text" className={styles.form_control} name="email" id="email" placeholder="Email" />
-                      </div>
-                    </Row>
-                    <Row>
-                      <div className={`col-md-12 ${styles.form_group}`}>
-                        <input type="text" className={styles.form_control} name="subject" id="subject" placeholder="Subject" />
-                      </div>
-                    </Row>
-                    <Row>
-                      <div className={`col-md-12 ${styles.form_group}`}>
-                        <textarea className={styles.form_control} name="message" id="message" cols="30" rows="7" placeholder="Write your message"></textarea>
-                      </div>
-                    </Row>
-                    <Row>
-                      <div className="col-12">
-                        <div className={styles.btn}>
-                          <input type="submit" value="Send Message" className={`${styles.btn_primary} rounded-0 py-2 px-4 `} />
-                          <span className="submitting"></span>
-                        </div>
-                      </div>
-                    </Row>
-                  </form>
-
-                  <div id={` ${styles.form_message_warning} mt-4`} ></div>
-                  <div id={styles.form_message_success}>
-                    Your message was sent, thank you!
-                  </div>
-                </div>
-              </Row>
-            </div>
-          </Row>
-        </Container>
-
-      </div>
+ 
+      <div>About</div>
+     
+    
 
     )
 }
