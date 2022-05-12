8d6c293c9435f24bbad1d9417ead1fae733a91f3
Fatjona <fatjonaash1@gmail.com>
2022-05-12 17:40:58 +0200

    updated Header and Navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Section, Button, Container, Row, } from 'react-bootstrap';
+import { section, Button, Container, Row, } from 'react-bootstrap';
 import style from './header.module.scss';
 
 function Header() {
@@ -11,7 +11,7 @@ function Header() {
             <Container>
                 <Row>
                     <div className="col-lg-7 col-md-12 col-12 d-flex align-items-center">
-                        <div className={style.about_text}>
+                      <div className={style.about_text}>
                             <small className={style.small_text}> To do lists
                                 <span className={style.mobile_block}> for busy people! </span>
                             </small >
@@ -19,17 +19,17 @@ function Header() {
                                 <span className="mr-2" > we help you to </span>
                                 <div className={style.animated_info}>{items && items.map((elem) => <span className={style.animated_item}>{elem}</span>)}</div>
                             </h1>
-                            <div className={`mt-4 ${style.custom_btn_group}`}>
-                                <a href="login" className={`btn ${[style.costum_btn, style.costum_btn_bg, style.costum_btn_link]}`}>
-                                    Create your to do list </a>
-                            </div>
+                            <div className={` ${style.custom_btn_group} && mt-4`}>
+                                    <a href="login" className={`btn ${style.custom_btn} && ${style.custom_btn_bg}  && ${style.custom_btn_link}`}>
+                                      Create your to do list </a></div>
+                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-5 col-md-12 col-12" >
-                        <div className={`${style.about_image} svg`}>
-                            <img src="../img/header.svg" className="img-fluid" alt="svg image" />
+                    
+                     <div className="col-lg-5 col-md-12 col-12" >
+                        <div className={`${style.about_image} && svg`}>
+                            <img src="../img/header.svg" className="img-fluid" alt="svg" />
                         </div>
-                    </div>
+                      </div>
 
                 </Row>
             </Container>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import Navstyle from './navbar.module.scss';
 
 const Menu = () => {
   return (
-    <div >
-      <Navbar collapseOnSelect expand="lg" bg="white" className={Navstyle.nav}>
-        <Container className={Navstyle.Container} >
-          <Navbar.Brand href="/home" > DoList </Navbar.Brand>
+    
+      <Navbar collapseOnSelect expand="lg" bg="white" className={`${Navstyle.nav} && navbar-expand-sm && navbar-light`}>
+        <Container>
+          <Navbar.Brand href="/home"> DoList </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav" >
             <Nav className="me-auto" >
@@ -19,10 +19,75 @@ const Menu = () => {
               <Nav.Link href="/profile" > Profile </Nav.Link>
               <Nav.Link eventKey={2} href="/about" > About </Nav.Link>
             </Nav>
+            <Nav>
+                <div className="ml-lg-4">
+                  <div className={` ${Navstyle.color_mode}&& d-lg-flex justify-content-center align-items-center` }>
+                    <i className={Navstyle.color_mode_icon}></i>
+                  </div>
+                </div>
+
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </div>
+
+
+/* <Nav className="navbar navbar-expand-sm navbar-light">
+    <Container>
+        <a className={}"navbar-brand" href="/home"><i className='uil uil-user'></i> DoList</a>
+
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+                <li className="nav-item">
+                    <a href="/newList" className="nav-link"><span data-hover="CreateList">New List</span></a>
+                </li>
+                <li className="nav-item">
+                    <a href="/myLists" className="nav-link"><span data-hover="myLists">My Lists</span></a>
+                </li>
+                <li className="nav-item">
+                    <a href="/Analytics" className="nav-link"><span data-hover="Analytics">Analytics</span></a>
+                </li>
+                <li className="nav-item">
+                    <a href="/profile" className="nav-link"><span data-hover="Profile">Contact</span></a>
+                </li>
+                <li className="nav-item">
+                    <a href="/about" className="nav-link"><span data-hover="About">About</span></a>
+                </li>
+            </ul>
+
+            <ul className="navbar-nav ml-lg-auto">
+                <div className="ml-lg-4">
+                  <div className="color-mode d-lg-flex justify-content-center align-items-center">
+                    <i className="color-mode-icon"></i>
+                    
+                  </div>
+                </div>
+            </ul>
+        </div>
+    </Container>
+</Nav> */
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
   )
 }
 
