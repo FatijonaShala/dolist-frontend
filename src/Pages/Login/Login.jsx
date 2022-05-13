09e715cb2350d7114caceebc09f99bfcaefb51ce
Fatjona <fatjonaash1@gmail.com>
2022-05-13 23:31:15 +0200

    login styling update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -33,17 +33,17 @@ function Login() {
               <Form className={` text-center w-1 ml-1 mt-5 pt-5 ${styles.contact_form}`} onSubmit={handleSubmit} >
                 <h3 className="mb-4" > Sign in </h3>
                 <Form.Group>
-                  <Form.Control type="username" placeholder="Username" className={`mb-3 ${styles.form_control}`} value={username}
+                  <Form.Control type="username" placeholder="Username" className={` ml-5 ${styles.form_control}`} value={username}
                     onChange={(u) => setUsername(u.target.value)} />
                 </Form.Group>
                 <Form.Group>
-                  <Form.Control type="password" placeholder="Password" className={`mb-3 ${styles.form_control}`} value={password}
+                  <Form.Control type="password" placeholder="Password" className={` ml-5 ${styles.form_control}`} value={password}
                     onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
-                <Form.Group className="d-flex justify-content-center mb-4">
+                <Form.Group className="d-flex justify-content-center mb-1">
                   <Form.Check label="Remember me" />
                 </Form.Group>
-                <div>
+               <div>
                   <Button type='submit' className={`btn ${styles.form_control} && ${styles.submit_btn}`} >Login</Button>
                 </div>
               </Form>
