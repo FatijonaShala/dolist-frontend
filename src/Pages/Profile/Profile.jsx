583667498c4141657fc3bda0a4680a6fba5d1be6
Fatjona <fatjonaash1@gmail.com>
2022-06-08 16:44:45 +0200

    update

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './profile.module.scss';
+import { Container, Row } from 'react-bootstrap';
 
 import LayoutWrapper from '../../hoc/Layout';
 
@@ -7,9 +8,18 @@ import LayoutWrapper from '../../hoc/Layout';
 
 function Profile() {
   return ( 
-    <div >
-      <h3>Profile</h3>
-    </div>
+    <div>
+  
+      <Container className="bootstrap snippets bootdey">
+        <Row>
+          Profile
+        </Row>
+    </Container>
+
+
+
+
+    </div >
 
 
   )
