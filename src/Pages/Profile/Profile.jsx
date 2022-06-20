0ae6d5763b6b76aabfd0d24bedbc3166500d3bd1
Fatjona <fatjonaash1@gmail.com>
2022-06-21 00:56:13 +0200

    profile update

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import style from "./profile.module.scss";
-import { Form, Button, Container, Row } from "react-bootstrap";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import LayoutWrapper from "../../hoc/Layout";
 
 function Profile() {
@@ -9,68 +9,77 @@ function Profile() {
   const [password, setPassword] = useState("Update Password");
 
   return (
-    <Container className=" rounded bg-white mt-5 mb-5">
+    <Container className="rounded bg-white mt-5 mb-5">
       <Row>
-        <div className="col-md-3 border-right">
+        <Col className="col-md-3 border-right">
           <div className="d-flex flex-column align-items-center text-center p-3 py-5">
             <img
               className="rounded-circle mt-5"
               width="150px"
               src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"
             />
-            <span className="font-weight-bold">{username}</span>
-            <span className="text-black-50">{email}</span>
+            <span className="mr-2 font-weight-bold">{username}</span>
+
+            <span className="mr-2 text-black-70">{email}</span>
             <span> </span>
           </div>
-        </div>
-        <div className="col-md-5 border-right">
+        </Col>
+        <Col className="col-md-5 border-right">
           <div className="p-3 py-5">
             <div className="d-flex justify-content-between align-items-center mb-3">
-              <h4 className="text-right">Profile Settings</h4>
+              <h3 className="text-right mb-4">Profile Settings</h3>
             </div>
-            <Form>
+            <Form className={style.contact_form}>
               <div className="row mt-2">
                 <div className="col-md-6">
                   <label className="labels">Username</label>
-                  <Form.Control type="text" placeholder="username" value="" />
+                  <Form.Control
+                    className={` ml-5 ${style.form_control}`}
+                    type="username"
+                    placeholder="username"
+                  />
                 </div>
                 <div className="col-md-6">
                   <label className="labels">Email</label>
-                  <Form.Control type="email" value="" placeholder="email" />
+                  <Form.Control
+                    className={` ml-5 ${style.form_control}`}
+                    type="email"
+                    placeholder="email"
+                  />
                 </div>
               </div>
               <div className="row mt-3">
                 <div className="col-md-12">
                   <label className="labels">Password</label>
                   <Form.Control
+                    className={` ml-5 ${style.form_control}`}
                     type="password"
-                    value=""
                     placeholder="password"
                   />
                 </div>
 
                 <div className="col-md-12">
-                  <br />
                   <label className="labels">Confirm Password</label>
                   <Form.Control
+                    className={` ml-5 ${style.form_control}`}
                     type="password"
-                    value=""
                     placeholder="confirm password"
                   />
                 </div>
               </div>
-            </Form>
+              {/* </Form> */}
 
-            <div className="mt-5 text-center">
-              <Button
-                type="submit"
-                className={`btn ${style.form_control} && ${style.submit_btn}`}
-              >
-                Save profile
-              </Button>
-            </div>
+              <div className="mt-5 text-center">
+                <Button
+                  type="submit"
+                  className={`btn ${style.form_control} && ${style.submit_btn}`}
+                >
+                  Update
+                </Button>
+              </div>
+            </Form>
           </div>
-        </div>
+        </Col>
       </Row>
     </Container>
   );
